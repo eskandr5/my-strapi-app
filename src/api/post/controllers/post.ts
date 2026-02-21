13b6f9f456bef7052626b258de4c80c8db85dcab
eskandr5 <mohammadeskandr5@gmail.com>
2026-02-21 21:53:17 +0300

    Fix: Add multipart support to post controller for image upload

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,30 +1,51 @@
 'use strict';
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { parseMultipartData } = require('@strapi/utils');
 
 module.exports = createCoreController('api::post.post', ({ strapi }) => ({
-    // دالة الإنشاء لدعم الصور والنصوص معاً
+
     async create(ctx) {
         let entity;
+
         if (ctx.is('multipart')) {
-            // هذه الجزئية هامة جداً لمعالجة الصور
             const { data, files } = parseMultipartData(ctx);
-            entity = await strapi.service('api::post.post').create({ data, files });
+            entity = await strapi.service('api::post.post').create({
+                data: { ...data, publishedAt: new Date() },
+                files
+            });
         } else {
             entity = await strapi.service('api::post.post').create(ctx.request.body);
         }
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-        return this.transformResponse(sanitizedEntity);
+
+        // استخدام super بدلاً من this لإيقاف التنبيهات الحمراء
+        const sanitizedEntity = await super.sanitizeOutput(entity, ctx);
+        return super.transformResponse(sanitizedEntity);
     },
 
-    // احتفظ بدوال find و findOne التي كتبناها سابقاً للـ Populate
     async find(ctx) {
-        ctx.query = { ...ctx.query, populate: { image: true, user: true, comments: { populate: { user: true } }, likes: { populate: { user: true } } } };
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                image: true,
+                user: { fields: ['username', 'email'] },
+                comments: { populate: { user: { fields: ['username'] } } },
+                likes: { populate: { user: { fields: ['username'] } } }
+            }
+        };
         return await super.find(ctx);
     },
 
     async findOne(ctx) {
-        ctx.query = { ...ctx.query, populate: { image: true, user: true, comments: { populate: { user: true } }, likes: { populate: { user: true } } } };
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                image: true,
+                user: { fields: ['username', 'email'] },
+                comments: { populate: { user: { fields: ['username'] } } },
+                likes: { populate: { user: { fields: ['username'] } } }
+            }
+        };
         return await super.findOne(ctx);
     }
 }));
