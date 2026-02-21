2bed3db32828b02e2569894fc3e9f9e1b665f1d2
eskandr5 <mohammadeskandr5@gmail.com>
2026-02-21 21:50:02 +0300

    Fix: Add multipart support to post controller for image upload

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -1,75 +1,30 @@
 'use strict';
 
-/**
- * post controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::post.post', ({ strapi }) => ({
-    async find(ctx) {
-        // تحديث الاستعلام ليشمل العلاقات العميقة
-        ctx.query = {
-            ...ctx.query,
-            populate: {
-                // جلب صورة المنشور
-                image: {
-                    populate: true
-                },
-                // جلب صاحب المنشور
-                user: {
-                    fields: ['username', 'email'] // يمكنك تحديد الحقول التي تريدها فقط للأمان
-                },
-                // جلب اللايكات مع بيانات المستخدم الذي قام باللايك
-                likes: {
-                    populate: {
-                        user: {
-                            fields: ['username']
-                        }
-                    }
-                },
-                // جلب التعليقات مع بيانات المستخدم الذي كتب التعليق
-                comments: {
-                    populate: {
-                        user: {
-                            fields: ['username']
-                        }
-                    }
-                }
-            },
-        };
+    // دالة الإنشاء لدعم الصور والنصوص معاً
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            // هذه الجزئية هامة جداً لمعالجة الصور
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.service('api::post.post').create({ data, files });
+        } else {
+            entity = await strapi.service('api::post.post').create(ctx.request.body);
+        }
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+        return this.transformResponse(sanitizedEntity);
+    },
 
-        const { data, meta } = await super.find(ctx);
-        return { data, meta };
+    // احتفظ بدوال find و findOne التي كتبناها سابقاً للـ Populate
+    async find(ctx) {
+        ctx.query = { ...ctx.query, populate: { image: true, user: true, comments: { populate: { user: true } }, likes: { populate: { user: true } } } };
+        return await super.find(ctx);
     },
 
     async findOne(ctx) {
-        ctx.query = {
-            ...ctx.query,
-            populate: {
-                image: {
-                    populate: true
-                },
-                user: {
-                    fields: ['username', 'email']
-                },
-                likes: {
-                    populate: {
-                        user: {
-                            fields: ['username']
-                        }
-                    }
-                },
-                comments: {
-                    populate: {
-                        user: {
-                            fields: ['username']
-                        }
-                    }
-                }
-            },
-        };
-        const { data, meta } = await super.findOne(ctx);
-        return { data, meta };
+        ctx.query = { ...ctx.query, populate: { image: true, user: true, comments: { populate: { user: true } }, likes: { populate: { user: true } } } };
+        return await super.findOne(ctx);
     }
 }));
