04cb91e21c300de6561be929418972f90faefb61
eskandr5 <mohammadeskandr5@gmail.com>
2026-02-23 01:53:24 +0300

    Fix: Update CORS origins to include localhost:5175

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -23,9 +23,9 @@ export default [
       origin: [
         'http://localhost:5173',
         'http://localhost:5174',
+        'http://localhost:5175', // المنفذ الذي تستخدمه حالياً
         'http://127.0.0.1:5173',
-        'http://127.0.0.1:5500',
-        'http://localhost:5500',
+        'http://127.0.0.1:5175', // للأمان أضف هذا أيضاً
         'https://eskandr5.github.io'
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
